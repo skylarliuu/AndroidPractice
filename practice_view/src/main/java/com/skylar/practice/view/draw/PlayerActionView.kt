4d6fdc2d5641890975faca275b149dfdfdd34927
lsq <siqian_liu@hpplay.cn>
2022-04-28 14:25:20 +0800

    practice_view: optimize PlayerActionView
    优化播放状态动画

--- practice_view/src/main/java/com/skylar/practice/view/draw/PlayerActionView.kt
+++ practice_view/src/main/java/com/skylar/practice/view/draw/PlayerActionView.kt
@@ -1,23 +1,28 @@
 package com.skylar.practice.view.draw
 
+import android.animation.PointFEvaluator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import com.skylar.practice.view.Utils
-import com.skylar.practice.view.draw.BezierUtil.CalculateBezierPointForQuadratic
 
 
-//
+// 仿喜马拉雅播放状态按钮切换
 // Created by skylar on 2022/4/27.
 //
 class PlayerActionView : View {
 
+    companion object {
+        private val BG_COLOR = Color.WHITE
+        private val RECT_COLOR = Color.parseColor("#FF65433A")
+    }
+
     private val mPaint = Paint()
     private val mLeftPath = Path()
     private val mRightPath = Path()
+
 
     private var mLeftStartOne = PointF(0f, 0f)
     private var mLeftStartTwo = PointF(0f, 0f)
@@ -28,11 +33,6 @@ class PlayerActionView : View {
     private var mLeftEndTwo = PointF(0f, 0f)
     private var mLeftEndThree = PointF(0f, 0f)
     private var mLeftEndFour = PointF(0f, 0f)
-
-    private var mLeftControlOne = PointF(0f, 0f)
-    private var mLeftControlTwo = PointF(0f, 0f)
-    private var mLeftControlThree = PointF(0f, 0f)
-    private var mLeftControlFour = PointF(0f, 0f)
 
     private var mLeftCurrentOne = PointF(0f, 0f)
     private var mLeftCurrentTwo = PointF(0f, 0f)
@@ -49,20 +49,27 @@ class PlayerActionView : View {
     private var mRightEndThree = PointF(0f, 0f)
     private var mRightEndFour = PointF(0f, 0f)
 
-    private var mRightControlOne = PointF(0f, 0f)
-    private var mRightControlTwo = PointF(0f, 0f)
-    private var mRightControlThree = PointF(0f, 0f)
-    private var mRightControlFour = PointF(0f, 0f)
-
     private var mRightCurrentOne = PointF(0f, 0f)
     private var mRightCurrentTwo = PointF(0f, 0f)
     private var mRightCurrentThree = PointF(0f, 0f)
     private var mRightCurrentFour = PointF(0f, 0f)
 
+    private val mLeftPointFEvaluatorOne = PointFEvaluator(mLeftCurrentOne)
+    private val mLeftPointFEvaluatorTwo = PointFEvaluator(mLeftCurrentTwo)
+    private val mLeftPointFEvaluatorThree = PointFEvaluator(mLeftCurrentThree)
+    private val mLeftPointFEvaluatorFour = PointFEvaluator(mLeftCurrentFour)
+
+    private val mRightPointFEvaluatorOne = PointFEvaluator(mRightCurrentOne)
+    private val mRightPointFEvaluatorTwo = PointFEvaluator(mRightCurrentTwo)
+    private val mRightPointFEvaluatorThree = PointFEvaluator(mRightCurrentThree)
+    private val mRightPointFEvaluatorFour = PointFEvaluator(mRightCurrentFour)
+
     private var mRectWidth = 0f
     private var mRectHeight = 0f
-
     private var mFraction = 0f
+
+    private var isPlay = true
+    private var valueAnimator : ValueAnimator? = null
 
     constructor(context: Context) : super(context)
 
@@ -74,9 +81,14 @@ class PlayerActionView : View {
     ) : super(context, attrs, defStyleAttr)
 
     init {
-        mPaint.color = Color.BLACK
         mPaint.style = Paint.Style.FILL
         mPaint.isAntiAlias = true
+
+        setOnClickListener {
+            //播放和暂停切换
+            isPlay = !isPlay
+            startAnimation()
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -90,52 +102,42 @@ class PlayerActionView : View {
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        mRectHeight = Utils.divide(width.toFloat(), 2f)
-        mRectWidth = Utils.divide(mRectHeight, 3f)
+        mRectHeight = Utils.divide(width.toFloat(), 3f)
+        mRectWidth = Utils.divide(mRectHeight, 4f)
+        val halfHeight = Utils.divide(mRectHeight, 2f)
 
-        mLeftStartOne.set(-mRectWidth * 1.5f, -mRectHeight / 2)
-        mLeftStartTwo.set(-mRectWidth * 0.5f, -mRectHeight / 2)
-        mLeftStartThree.set(-mRectWidth * 0.5f, mRectHeight / 2)
-        mLeftStartFour.set(-mRectWidth * 1.5f, mRectHeight / 2)
+        mLeftStartOne.set(-mRectWidth * 1.5f, -halfHeight)
+        mLeftStartTwo.set(-mRectWidth * 0.5f, -halfHeight)
+        mLeftStartThree.set(-mRectWidth * 0.5f, halfHeight)
+        mLeftStartFour.set(-mRectWidth * 1.5f, halfHeight)
 
-        mLeftEndOne.set(mRectWidth * 1.5f, 0f)
-        mLeftEndTwo.set(mRectWidth * 1.5f, 0f)
-        mLeftEndThree.set(-mRectWidth * 1.5f, 0f)
-        mLeftEndFour.set(-mRectWidth * 1.5f, -mRectHeight / 2)
+        //三角形的重心和圆心重合, 三角形才居中
+        val halfTriangleHeight = Utils.divide(mRectHeight, 3f)
+        mLeftEndOne.set(0f, -halfTriangleHeight * 2)
+        mLeftEndTwo.set(0f, -halfTriangleHeight * 2)
+        mLeftEndThree.set(0f, halfTriangleHeight)
+        mLeftEndFour.set(-mRectHeight * 0.5f, halfTriangleHeight)
 
         mLeftCurrentOne.set(mLeftStartOne.x, mLeftStartOne.y)
         mLeftCurrentTwo.set(mLeftStartTwo.x, mLeftStartTwo.y)
         mLeftCurrentThree.set(mLeftStartThree.x, mLeftStartThree.y)
         mLeftCurrentFour.set(mLeftStartFour.x, mLeftStartFour.y)
 
-        mLeftControlOne.set(0f, - (height / 2).toFloat())
-        mLeftControlTwo.set(0f, -(height / 2).toFloat())
-        mLeftControlThree.set(mLeftStartThree.x, mLeftStartThree.y)
-        mLeftControlFour.set((-width / 2).toFloat(),  0f)
+        mRightStartOne.set(mRectWidth * 0.5f, -halfHeight)
+        mRightStartTwo.set(mRectWidth * 1.5f, -halfHeight)
+        mRightStartThree.set(mRectWidth * 1.5f, halfHeight)
+        mRightStartFour.set(mRectWidth * 0.5f, halfHeight)
 
-
-
-        mRightStartOne.set(mRectWidth * 0.5f, -mRectHeight / 2)
-        mRightStartTwo.set(mRectWidth * 1.5f, -mRectHeight / 2)
-        mRightStartThree.set(mRectWidth * 1.5f, mRectHeight / 2)
-        mRightStartFour.set(mRectWidth * 0.5f, mRectHeight / 2)
-
-        mRightEndOne.set(mRectWidth * 1.5f, 0f)
-        mRightEndTwo.set(mRectWidth * 1.5f, 0f)
-        mRightEndThree.set(-mRectWidth * 1.5f, mRectHeight / 2)
-        mRightEndFour.set(-mRectWidth * 1.5f, 0f)
+        mRightEndOne.set(0f, -halfTriangleHeight * 2)
+        mRightEndTwo.set(0f, -halfTriangleHeight * 2)
+        mRightEndThree.set(mRectHeight * 0.5f, halfTriangleHeight)
+        mRightEndFour.set(0f, halfTriangleHeight)
 
         mRightCurrentOne.set(mRightStartOne.x, mRightStartOne.y)
         mRightCurrentTwo.set(mRightStartTwo.x, mRightStartTwo.y)
         mRightCurrentThree.set(mRightStartThree.x, mRightStartThree.y)
         mRightCurrentFour.set(mRightStartFour.x, mRightStartFour.y)
 
-        mRightControlOne.set(mRightStartOne.x, mRightStartOne.y)
-        mRightControlTwo.set(mRightStartTwo.x,mRightStartTwo.y)
-        mRightControlThree.set(0f, (height / 2).toFloat())
-        mRightControlFour.set(-mRectWidth*1.5f, mRectHeight / 2)
-
-        startAnimation()
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -145,6 +147,21 @@ class PlayerActionView : View {
         }
 
         canvas.translate((width / 2).toFloat(), (height / 2).toFloat())
+        //画背景圆圈
+        mPaint.color = BG_COLOR
+        canvas.drawCircle(0f, 0f, (width / 2).toFloat(), mPaint)
+
+        //动画过程中，旋转圆，这样矩形的四个点位置比较好计算，而且两个矩形的变化可以对称
+        if(valueAnimator?.isRunning == true || mFraction > 0) {
+            if(!isPlay) {
+                canvas.rotate(90 * mFraction)
+            } else if(isPlay){
+                canvas.rotate(90 + 90 * mFraction)
+            }
+        }
+
+        //画两个path，分别由四个点构成，动画不断改变四个点的位置
+        mPaint.color = RECT_COLOR
         mLeftPath.reset()
         mLeftPath.moveTo(mLeftCurrentOne.x, mLeftCurrentOne.y)
         mLeftPath.lineTo(mLeftCurrentTwo.x, mLeftCurrentTwo.y)
@@ -160,69 +177,47 @@ class PlayerActionView : View {
         mRightPath.lineTo(mRightCurrentFour.x, mRightCurrentFour.y)
         mRightPath.close()
         canvas.drawPath(mRightPath, mPaint)
-
     }
 
     private fun startAnimation() {
-        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
-        valueAnimator.duration = 2000
-        valueAnimator.repeatCount= ValueAnimator.INFINITE
-        valueAnimator.addUpdateListener {
+        valueAnimator?.end()
+
+        valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+        valueAnimator?.duration = 500
+        valueAnimator?.addUpdateListener {
             mFraction = it.animatedFraction
-            computePoint()
+            if(!isPlay) {
+                computePausePoint()
+            } else {
+                computePlayPoint()
+            }
             invalidate()
         }
-        valueAnimator.start()
+        valueAnimator?.start()
     }
 
-    private fun computePoint() {
-        mLeftCurrentOne = CalculateBezierPointForQuadratic(
-            mFraction,
-            mLeftStartOne, mLeftControlOne, mLeftEndOne
-        )
-        mLeftCurrentTwo = CalculateBezierPointForQuadratic(
-            mFraction,
-            mLeftStartTwo, mLeftControlTwo, mLeftEndTwo
-        )
-        mLeftCurrentThree = CalculateBezierPointForQuadratic(
-            mFraction,
-            mLeftStartThree, mLeftControlThree, mLeftEndThree
-        )
-        mLeftCurrentFour = CalculateBezierPointForQuadratic(
-            mFraction,
-            mLeftStartFour, mLeftControlFour, mLeftEndFour
-        )
 
-        mRightCurrentOne = CalculateBezierPointForQuadratic(
-            mFraction,
-            mRightStartOne, mRightControlOne, mRightEndOne
-        )
-        mRightCurrentTwo = CalculateBezierPointForQuadratic(
-            mFraction,
-            mRightStartTwo, mRightControlTwo, mRightEndTwo
-        )
-        mRightCurrentThree = CalculateBezierPointForQuadratic(
-            mFraction,
-            mRightStartThree, mRightControlThree, mRightEndThree
-        )
-        mRightCurrentFour = CalculateBezierPointForQuadratic(
-            mFraction,
-            mRightStartFour, mRightControlFour, mRightEndFour
-        )
+    private fun computePausePoint() {
+        mLeftPointFEvaluatorOne.evaluate(mFraction, mLeftStartOne, mLeftEndOne)
+        mLeftPointFEvaluatorTwo.evaluate(mFraction, mLeftStartTwo, mLeftEndTwo)
+        mLeftPointFEvaluatorThree.evaluate(mFraction, mLeftStartThree, mLeftEndThree)
+        mLeftPointFEvaluatorFour.evaluate(mFraction, mLeftStartFour, mLeftEndFour)
+
+        mRightPointFEvaluatorOne.evaluate(mFraction, mRightStartOne, mRightEndOne)
+        mRightPointFEvaluatorTwo.evaluate(mFraction, mRightStartTwo, mRightEndTwo)
+        mRightPointFEvaluatorThree.evaluate(mFraction, mRightStartThree, mRightEndThree)
+        mRightPointFEvaluatorFour.evaluate(mFraction, mRightStartFour, mRightEndFour)
     }
 
-    private fun printPoint() {
-        Log.i("PlayerActionView", "one : ${mLeftStartOne.x}, ${mLeftStartOne.y}")
-        Log.i("PlayerActionView", "two : ${mLeftStartTwo.x}, ${mLeftStartTwo.y}")
-        Log.i("PlayerActionView", "three : ${mLeftStartThree.x}, ${mLeftStartThree.y}")
-        Log.i("PlayerActionView", "four : ${mLeftStartFour.x}, ${mLeftStartFour.y}")
+    private fun computePlayPoint() {
+        mLeftPointFEvaluatorOne.evaluate(mFraction, mLeftEndOne, mLeftStartOne)
+        mLeftPointFEvaluatorTwo.evaluate(mFraction, mLeftEndTwo, mLeftStartTwo)
+        mLeftPointFEvaluatorThree.evaluate(mFraction, mLeftEndThree, mLeftStartThree)
+        mLeftPointFEvaluatorFour.evaluate(mFraction, mLeftEndFour, mLeftStartFour)
+
+        mRightPointFEvaluatorOne.evaluate(mFraction, mRightEndOne, mRightStartOne)
+        mRightPointFEvaluatorTwo.evaluate(mFraction, mRightEndTwo, mRightStartTwo)
+        mRightPointFEvaluatorThree.evaluate(mFraction, mRightEndThree, mRightStartThree)
+        mRightPointFEvaluatorFour.evaluate(mFraction, mRightEndFour, mRightStartFour)
     }
 }
-
-
-//class BezierEvaluator(private val mControlPoint: PointF) :
-//    TypeEvaluator<PointF> {
-//    override fun evaluate(t: Float, startValue: PointF, endValue: PointF): PointF {
-//        return BezierUtil.CalculateBezierPointForQuadratic(t, startValue, mControlPoint, endValue)
-//    }
-//}
